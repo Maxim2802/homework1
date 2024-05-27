c7b145c0aa06b3e2cc0e8e31257568f7fca77728
Maxim2802 <maximtchebykin@yandex.ru>
2024-05-28 01:46:38 +0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Тапинамбур'
+print(example[0])
+print(example[9])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
